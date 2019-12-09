0765158a217b7e5653555c1a0b68dbffd1bfba97
Ignorance-of-Dong <1054914660@qq.com>
2019-12-09 10:01:01 +0800

    '删除打包资源文件'

--- public/js/src/crash_cheat.js
+++
@@ -1,1 +0,0 @@
-var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="Web page loss ~",clearTimeout(titleTime)):(document.title="(ฅ>ω<*ฅ) ~"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
